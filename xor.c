474b1c3d72b4cc27428cdae0d99a56f0a3849c24
Gerardo Magela Machado da Silva <magelace@yahoo.com.br>
2022-06-24 13:15:28 -0300

    XOR

---
+++ xor.c
@@ -0,0 +1,18 @@
+// Exemplo XOR (OR exclusivo)
+#include <stdio.h>
+
+int xor(int a, int b);
+
+void main(void)
+{
+    printf("%d", xor(1, 0));
+    printf("%d", xor(1, 1));
+    printf("%d", xor(0, 1));
+    printf("%d", xor(0, 0));
+}
+
+/* Executa uma operação lógica XOR usando os dois argumentos. */
+int xor(int a, int b)
+{
+    return (a || b) && !(a && b);
+}
