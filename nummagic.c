2fec7a02ea4c03648c8bfdfa50d3fdab81ce3fcd
Gerardo Magela Machado da Silva <magelace@yahoo.com.br>
2022-06-24 18:23:29 -0300

    Número Mágico #3

--- nummagic.c
+++ nummagic.c
@@ -1,4 +1,4 @@
-/* Programa de números mágicos #2. */
+/* Programa de números mágicos #3. */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,6 +12,13 @@ void main(void)
     printf("adivinhe o número mágico: ");
     scanf("%d", &guess);
 
-    if (guess == magic) printf("** Certo **");
-    else printf("Errado");
+    if (guess == magic) {
+        printf("** Certo **");
+        printf(" %d é número mágico\n", magic);
+    }
+    else {
+        printf("Errado, ");
+        if (guess > magic) printf("muito alto\n");
+        else printf("muito baixo\n");
+    }
 }
