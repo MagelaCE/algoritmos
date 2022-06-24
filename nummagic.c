ca6454d5f08e890d82025aaabb573c64c3bc0032
Gerardo Magela Machado da Silva <magelace@yahoo.com.br>
2022-06-24 18:08:39 -0300

    Número Mágico #1

---
+++ nummagic.c
@@ -0,0 +1,16 @@
+/* Programa de números mágicos #1. */
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+    int magic;  /* número mágico */
+    int guess;  /* palpite do número */
+
+    magic = rand();     /* gera o número mágico */
+
+    printf("adivinhe o número mágico: ");
+    scanf("%d", &guess);
+
+    if (guess == magic) printf("** Certo **");
+}
