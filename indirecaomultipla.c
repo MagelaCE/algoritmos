7d609a8fd7daa871e37cb46a81dfae70a51706fb
Gerardo Magela Machado da Silva <magelace@yahoo.com.br>
2022-06-26 06:47:02 -0300

    Indireção Múltipla

---
+++ indirecaomultipla.c
@@ -0,0 +1,13 @@
+/* Exemplo de indireção múltipla */
+#include <stdio.h>
+
+void main(void)
+{
+    int x, *p, **q;
+
+    x = 10;
+    p = &x;
+    q = &p;
+
+    printf("%d", **q);  /* imprime o valor de x */
+}
