b3fd3d6028ee3da4fb19d749059aa508111f749b
Gerardo Magela Machado da Silva <magelace@yahoo.com.br>
2022-06-26 06:32:55 -0300

    Atribuição de Ponteiro

---
+++ atribuicaoponteiro.c
@@ -0,0 +1,13 @@
+/* Exemplo de atribuição de ponteiro */
+#include <stdio.h>
+
+void main(void)
+{
+    int x;
+    int *p1, *p2;
+
+    p1 = &x;
+    p2 = p1;
+
+    printf("%p", p2);   /* escreve o endereço de x, não seu valor! */
+}
